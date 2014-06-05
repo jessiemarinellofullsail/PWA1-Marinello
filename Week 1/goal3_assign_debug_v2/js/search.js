29c0d8cc7c2d8e0974eb10c5e34b6dd5455e9c81
Jessie Marinello <jessiemarinello@fullsail.edu>
2014-06-04 22:15:45 -0400

    complete pseudo code

--- Week 1/goal3_assign_debug_v2/js/search.js
+++ Week 1/goal3_assign_debug_v2/js/search.js
@@ -43,79 +43,82 @@
         var queryArray = query.join(" "); // I THINK that this joins an array into string using given separator, " ". --NOTE-- This should probably be .split
 
         // array to store matched results from database.js
-        var results = []; //create results variable to store matched results from database
+        var results = []; //create results variable to store matched results from database; store result
 
         // loop through each index of db (database.js) array
         for(var i=0, j=db.length; i<j; i++){
 
             // each db[i] is a single video item, each title ends with a pipe "|"
             // save a lowercase variable of the video title
-            var dbTitleEnd = db[i].indexOf('|'); //indexOf() method returns the index within the calling String object of the first occurrence of the specified value, starting the search at fromIndex,returns -1 if the value is not found. --NOTE-- I'm not sure WHY the pipe is being called. I think the db[i] is being searched to locate values that match the query
+            var dbTitleEnd = db[i].indexOf('|'); //indexOf() method returns the index within the calling String object of the first occurrence of the specified value, starting the search at fromIndex,returns -1 if the value is not found. --NOTE-- I'm not sure WHY the pipe is being called. I think the db[i] is being searched to locate values that match the query; store result
 
-            var dbitem = db[i].tolowercase().substring(0, dbTitleEnd);
+            var dbitem = db[i].tolowercase().substring(0, dbTitleEnd); //--NOTE--is this like case mapping? Is there a method for such? Examine individual characters of the sequence, creating a copy of a string with all characters translated to lowercase.; BUT WHY??? Is it intended to yield the correct result???; store result
+            //--NOTE-- above variable should be camel case dbItem
 
             // loop through the user's search query words
             // save a lowercase variable of the search keyword
             for(var ii=0, jj=queryArray.length; ii<jj; ii++){
-                var qitem = queryArray[ii].tolowercase();
+                var qitem = queryArray[ii].tolowercase(); //I think this needs to be lower case so that the user matches the host
+                //--NOTE-- above variable should be camel case qItem, even better would be queryItem
 
                 // is the keyword anywhere in the video title?
                 // If a match is found, push full db[i] into results array
-                var compare = dbitem.indexOf(qitem);
-                if(compare !== -1){
-                    results.push(db[i]);
+                var compare = dbitem.indexOf(qitem); //--NOTE--change dbitem and qitem to camel case
+                if(compare !== -1){ //If it is not equal to -1, than I believe that the item CAN be found, which should push the database results to results
+                    results.push(db[i]); //items are pushed into the db array index
                 };
-                ;
+                ; //Unnecessary semi colons?
                 ;
 
-                results.sort();
+                results.sort(); //sort the results array (alphabetically?)
 
                 // Check that matches were found, and run output functions
-                if(results.length = 0){ //--NOTE-- This is setting results.length to 0?
-                    noMatch();
-                }else{
-                    showMatches(results);
-                };
-            };
+                if(results.length = 0){ //--NOTE-- This is setting results.length to 0? If the length of the results is equal to 0, then there are no results in the database
+                    noMatch(); //set no match argument
+                }else{ //ELSE if there is a match, ie results.length >= 1, will be placed into showMatches
+                    showMatches(results); //set show match argument
+                }; //Unnecessary semi colons
+            }; //Unnecessary semi colons
 
             // Put "No Results" message into page (DO NOT FIX THE HTML VAR NOR THE innerHTML)
-            var noMatch = function(){
+            var noMatch = function(){ //create function that displays "No Results" into HTML if the above if statement is met
                 var html = ''+
                         '<p>No Results found.</p>'+
                         '<p style="font-size:10px;">Try searching for "JavaScript".  Just an idea.</p>'
                     ;
-                resultsDIV.innerHTML = html;
+                resultsDIV.innerHTML = html; //DOM sets the inner HTML code of an HTML element to dynamically change page.
             };
 
             // Put matches into page as paragraphs with anchors
-            var showMatches = function(results){
+            var showMatches = function(results){ //when above ELSE statement for show matches is met, apply function to show user results of their search
 
                 // THE NEXT 4 LINES ARE CORRECT.
-                var html = '<p>Results</p>',
+                var html = '<p>Results</p>',//use html, title, and url variables from db to render on html page
                     title,
                     url
                     ;
 
                 // loop through all the results search() function
-                for(var i=0, j=results.length; i<j; i++){
+                for(var i=0, j=results.length; i<j; i++){ //CONFUSED on this loop and what it is for!!! Think it has something to do with the video links that can be accessed once the user has selected a search result from the returned list of results. Trying to figure out WHY it is needed. Pulling the correct video for the search result that was selected by the user???
 
                     // title of video ends with pipe
                     // pull the title's string using index numbers
-                    titleEnd = results[i].indexOf('|');
+                    titleEnd = results[i].indexOf('|'); //I'd really appreciate being able to see a working copy of this script to better understand what happens once a user has a returned list of search results
                     title = results[i].subString(0, titleEnd);
 
                     // pull the video url after the title
                     url = results[i].substring(results[i].indexOf('|')+1, results[i].length);
 
                     // make the video link - THE NEXT LINE IS CORRECT.
-                    html += '<p><a href=' + url + '>' + title + '</a></p>';
+                    html += '<p><a href=' + url + '>' + title + '</a></p>'; //add to the html the video link and title. Trying to figure out the greater than symbol here
                 };
                 resultsDIV.innerHTML = html; //THIS LINE IS CORRECT.
+                //change the innerHTML of Div in html
             };
 
             // The onsubmit event will be reviewed in upcoming Course Material.
             // THE LINE DIRECTLY BELOW IS CORRECT
-            document.forms[0].onsubmit = function(){
+            document.forms[0].onsubmit = function(){ //calling the submit event
                 var query = searchInput.value;
                 validqte(query); //--NOTE-- Should be var validate
 
