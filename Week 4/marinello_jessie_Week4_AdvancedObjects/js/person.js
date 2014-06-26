738e0f59f1a1d118b698fb4a4cdd377cfc1d5728
Jessie Marinello <jessiemarinello@fullsail.edu>
2014-06-26 11:50:59 -0400

    Begin creating constructor functions for person.js (this is where my advanced objects & prototypes live)

--- Week 4/marinello_jessie_Week4_AdvancedObjects/js/person.js
+++ Week 4/marinello_jessie_Week4_AdvancedObjects/js/person.js
@@ -2,3 +2,44 @@
 //Week 4 Advanced Objects
 //June 2014
 //person.js - holds advanced objects "blueprint"
+
+//create self executing function
+(function(){
+
+//add the Person object to the global window object because main.js requires it
+    window.Person = Person;
+
+//define all possible job choices and actions for all the Person instances
+    Person.jobs = ["teacher", "actor", "student", "scuba diver"];
+    Person.actions = ["sleeping", "eating", "working"];
+
+    function Person(name,row){
+        console.log("Person created: ", name);
+
+        this.name = name;
+
+        //sets initial action for each person randomly
+        this.action = Person.actions[Math.floor(Math.random()*Person.actions.length)];
+
+        //sets job per person randomly
+        this.job = Person.jobs[Math.floor(Math.random()*Person.jobs.length)];
+
+        //defines row so JS knows what row in the HTML to update text in
+        this.row = row;
+
+        //displays initial action
+        var id = document.getElementById("r"+this.row+"c3");
+        id.innerHTML = this.action;
+    }
+
+//Person's update method. This function occurs 30 times a second and randomly determines if the action changes for each person
+    Person.prototype.update = function(){
+        if (Math.floor(Math.random() <.01)){
+            var i = Math.floor(Math.random()*Person.actions.length);
+        }
+    }
+
+
+
+}
+
