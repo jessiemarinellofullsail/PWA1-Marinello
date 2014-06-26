2c5062718f16dcb01b762ee7e49ee5148bcfa773
Jessie Marinello <jessiemarinello@fullsail.edu>
2014-06-26 11:58:31 -0400

    Continue work on setting up constructor function in person.js; add comments to identify “this” keyword and function parameters

--- Week 4/marinello_jessie_Week4_AdvancedObjects/js/person.js
+++ Week 4/marinello_jessie_Week4_AdvancedObjects/js/person.js
@@ -13,18 +13,23 @@
     Person.jobs = ["teacher", "actor", "student", "scuba diver"];
     Person.actions = ["sleeping", "eating", "working"];
 
-    function Person(name,row){
+//constructor function
+    function Person(name,row){ //pass parameters
         console.log("Person created: ", name);
 
+        //"this" keyword to define name
         this.name = name;
 
         //sets initial action for each person randomly
+        //"this" keyword to define actions
         this.action = Person.actions[Math.floor(Math.random()*Person.actions.length)];
 
         //sets job per person randomly
+        //"this" keyword to define jobs
         this.job = Person.jobs[Math.floor(Math.random()*Person.jobs.length)];
 
         //defines row so JS knows what row in the HTML to update text in
+        //"this" keyword to define row
         this.row = row;
 
         //displays initial action
@@ -36,10 +41,10 @@
     Person.prototype.update = function(){
         if (Math.floor(Math.random() <.01)){
             var i = Math.floor(Math.random()*Person.actions.length);
+            this.action = Person.actions[i];
+            var id = document.getElementById("r"+this.row+"c3");
+            id.innerHTML = this.action;
         }
     }
-
-
-
-}
+})();
 
