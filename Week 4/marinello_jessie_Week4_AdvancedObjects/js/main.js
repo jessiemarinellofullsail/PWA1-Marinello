388c8dbc46e23e1735142454bc409483e4b2d7b2
Jessie Marinello <jessiemarinello@fullsail.edu>
2014-06-26 11:34:36 -0400

    Set up Week 4, Advanced Objects directories; include js files to include header with name, assignment, date

---
+++ Week 4/marinello_jessie_Week4_AdvancedObjects/js/main.js
@@ -0,0 +1,4 @@
+//Jessie Marinello
+//Week 4 Advanced Objects
+//June 2014
+//main.js - holds all other js functions, instances, arrays
---
+++ Week 4/marinello_jessie_Week4_AdvancedObjects/js/person.js
@@ -0,0 +1,4 @@
+//Jessie Marinello
+//Week 4 Advanced Objects
+//June 2014
+//person.js - holds advanced objects "blueprint"
